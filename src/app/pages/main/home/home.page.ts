9edc4ca73e3d1dc78fde510733d8ae5019851c66
josue-mdz <josue10718@gmail.com>
2023-11-17 16:57:56 -0600

    MenuAct

--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -5,11 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage  {
+  isAlertOpen = false;
+  alertButtons = ['Cerrar', 'Salir'];
+  router: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+    
   }
 
+  handleAlertButtonClick(buttonIndex: number) {
+    if (buttonIndex === 0) {
+      // Acción para el botón 'Cerrar' (puedes dejarlo vacío o agregar lógica adicional)
+    } else if (buttonIndex === 1) {
+      // Acción para el botón 'Volver al Login'
+      this.router.navigate(['/auth']); // Ajusta '/login' según la ruta de tu componente Auth
+    }
+  }
 }
