cd98cc69ddbd15bf01f9f544ad31cb81b18c3967
josue-mdz <josue10718@gmail.com>
2023-11-29 17:00:03 -0600

    Hero

--- src/app/pages/main/home/home.module.ts
+++ src/app/pages/main/home/home.module.ts
@@ -8,15 +8,19 @@ import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 @NgModule({
     declarations: [HomePage],
+    schemas:[CUSTOM_ELEMENTS_SCHEMA],
     imports: [
         CommonModule,
         FormsModule,
         IonicModule,
         HomePageRoutingModule,
         SharedModule
-    ]
+    ],
+   
 })
 export class HomePageModule {}
