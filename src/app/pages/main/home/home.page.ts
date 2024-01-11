da57a90546e8c854ddad96539a47f5f95af92e9b
josue-mdz <josue10718@gmail.com>
2024-01-11 13:45:16 -0600

    Dis hero slide

--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Post } from 'src/app/models/post.model';
 import { UtilsService } from 'src/app/services/utils.service';
 import { Posts } from 'src/assets/data/images';
@@ -7,9 +7,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { AlertController } from '@ionic/angular';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { register } from 'swiper/element';
+import { Swiper } from 'swiper/types';
 
+register();
 
-declare  var Email: any;
+declare var Email: any;
 
 @Component({
   selector: 'app-home',
@@ -24,15 +27,29 @@ export class HomePage implements OnInit {
     nombre: '',
     correo: '',
     to_name: 'Admin',
-    message:'MENSAJE IMPORTANTE',
-    telefono: ''
+    message: 'MENSAJE IMPORTANTE',
+    telefono: '',
   };
+
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+
+  images2 = ['assets/fondo.webp', 'assets/Lerdop2.jpg', 'assets/Lerdop3.jpg'];
 
   constructor(
     private utilsSvc: UtilsService,
     private AuthService: AuthService,
     private alertController: AlertController
   ) {}
+
+  SwiperReady() {
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+
+  swiperSlideChanged(e: any) {
+    console.log('changed: ', e);
+  }
 
   ngOnInit() {}
 
@@ -68,15 +85,18 @@ export class HomePage implements OnInit {
   formatoCelularContacto(phoneNumber: string): string {
     // Remover cualquier caracter que no sea un número
     phoneNumber = phoneNumber.replace(/[^\d]/g, '');
-  
+
     if (phoneNumber.length >= 10) {
-      phoneNumber = `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6)}`;
+      phoneNumber = `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+        3,
+        6
+      )}-${phoneNumber.slice(6)}`;
     }
-  
+
     if (phoneNumber.length >= 14) {
       phoneNumber = phoneNumber.slice(0, 14);
     }
-  
+
     return phoneNumber;
   }
   validateEmail(email) {
@@ -86,7 +106,7 @@ export class HomePage implements OnInit {
     console.log(re.test(String(email).toLowerCase()));
     return re.test(String(email).toLowerCase());
   }
-  async alert(title:  string, msg: string) {
+  async alert(title: string, msg: string) {
     const alert = this.alertController.create({
       header: title,
       message: msg,
@@ -95,48 +115,60 @@ export class HomePage implements OnInit {
     (await alert).present();
   }
   async sendemail() {
-    if (this.form.correo === '' || this.form.telefono === '' || this.form.nombre === '') {
-        this.alert('Error', 'Favor de llenar todos los datos');
-        return false;
+    if (
+      this.form.correo === '' ||
+      this.form.telefono === '' ||
+      this.form.nombre === ''
+    ) {
+      this.alert('Error', 'Favor de llenar todos los datos');
+      return false;
     }
 
     if (this.form.nombre.length < 10) {
       this.alert('Error', 'El nombre debe tener al menos 9 caracteres');
       return;
-  }
+    }
     if (String(this.form.telefono).length < 10) {
-        this.alert('Error', 'El teléfono debe ser de al menos 10 dígitos');
-        return;
+      this.alert('Error', 'El teléfono debe ser de al menos 10 dígitos');
+      return;
     }
     if (!this.validateEmail(this.form.correo)) {
-        this.alert('Error', 'El correo no es válido');
-        return;
+      this.alert('Error', 'El correo no es válido');
+      return;
     }
 
     const templateParams = {
-        to_name: this.form.to_name,
-        from_name: this.form.nombre,
-        from_email : this.form.correo,
-        phone: this.form.telefono,
-        message: this.form.message
+      to_name: this.form.to_name,
+      from_name: this.form.nombre,
+      from_email: this.form.correo,
+      phone: this.form.telefono,
+      message: this.form.message,
     };
 
-    emailjs.send('service_77zc03i', 'template_hvsbn1k', templateParams, '87TM-X1oVUlPZqwc_')
-    .then((res: EmailJSResponseStatus) => {
-        console.log('SUCCESS!', res.status, res.text);
-        this.alert('Éxito', 'Mensaje enviado correctamente');
-    }, (error) => {
-        console.error('FAILED...', error);
-        this.alert('Error', 'Error al enviar el mensaje: ' + error.text);
-    });
-}
+    emailjs
+      .send(
+        'service_77zc03i',
+        'template_hvsbn1k',
+        templateParams,
+        '87TM-X1oVUlPZqwc_'
+      )
+      .then(
+        (res: EmailJSResponseStatus) => {
+          console.log('SUCCESS!', res.status, res.text);
+          this.alert('Éxito', 'Mensaje enviado correctamente');
+        },
+        (error) => {
+          console.error('FAILED...', error);
+          this.alert('Error', 'Error al enviar el mensaje: ' + error.text);
+        }
+      );
+  }
 
-/* emailjs.send("service_77zc03i","template_hvsbn1k",{
+  /* emailjs.send("service_77zc03i","template_hvsbn1k",{
   from_name: "Lerdo",
   to_name: "test",
   from_email: "digitallerdo@gmail.com",
   phone: "8714706448",
   message: "hola",
   }); */
-
 }
